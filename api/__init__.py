e20ae5b0ee45915466bd6a3da5ef5e16e21b2f8c
Abubakar <alaroabubakarolayemi@yahoo.com>
2021-03-24 10:06:30 +0100

    api files added

---
+++ api/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.getcwd()}/todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello():
+	return 'Hello!'
